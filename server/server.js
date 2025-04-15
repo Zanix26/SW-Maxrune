df3f739d0b8a63fa3c82c5052bdb6d9544c91f61
Zanix26 <kamillanz@gmx.net>
2025-04-15 02:06:17 +0200

    server.js aktualisieren

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const apiRoutes = require('./routes/api');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/auth', authRoutes);
+
+mongoose.connect('mongodb://localhost/sw_maxrune', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB verbunden'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
