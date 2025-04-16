23546216d21edfb99eafdc8cdb8758e71efdeb9b
Zanix26 <kamillanz@gmx.net>
2025-04-17 00:39:08 +0200

    Delete server directory

--- server/models/Json.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const JsonSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    data: { type: Object, required: true },
-    uploadedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Json', JsonSchema);
