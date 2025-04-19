faaf010dc91cf63a07c602ca7082dc9b4d3491e6
Zanix26 <kamillanz@gmx.net>
2025-04-20 00:19:20 +0200

    user.js aktualisieren

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, uniquie: true },
-    email: { type: String, required: true, uniquie: true },
+    username: { type: String, required: true, unique: true }, // Korrektur: uniquie -> unique
+    email: { type: String, required: true, unique: true },    // Korrektur: uniquie -> unique
     password: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
 });
