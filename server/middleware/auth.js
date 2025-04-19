5ab15dade46b127eefec12292452b8165c54aa5c
Zanix26 <kamillanz@gmx.net>
2025-04-20 00:33:20 +0200

    auth.js aktualisieren

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,15 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer', '');
-    if(!token) return
-    res.status(401).json({ message: 'Kein Token bereitgestellt.' });
+    const token = req.header('Authorization')?.replace('Bearer ', ''); // Korrektur: Leerzeichen nach "Bearer"
+    if (!token) {
+        return res.status(401).json({ message: 'Kein Token bereitgestellt.' });
+    }
 
-    try{
-        const decoded = jwt.verify(token, 'your_jwt_secret');
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your_jwt_secret');
         req.userId = decoded.userId;
         next();
-    } catch (error){
+    } catch (error) {
         res.status(401).json({ message: 'Ungültiger Token.', error });
     }
 };
