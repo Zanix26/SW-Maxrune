bc06eb15b14f73dd0c358e8fd92103f6a7a47b86
Zanix26 <kamillanz@gmx.net>
2025-04-20 16:30:32 +0200

    monster.js aktualisieren

--- server/models/monster.js
+++ server/models/monster.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const effectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String }, // Entferne required: true
   is_buff: { type: Boolean },
   type: { type: String },
   description: { type: String },
@@ -13,7 +13,7 @@ const skillSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
   multiplier_formula: { type: String },
-  effects: [effectSchema], // Angepasstes Schema für effects
+  effects: [effectSchema],
 });
 
 const monsterSchema = new mongoose.Schema({
@@ -22,7 +22,7 @@ const monsterSchema = new mongoose.Schema({
   element: { type: String, enum: ['Water', 'Fire', 'Wind', 'Light', 'Dark'] },
   archetype: { type: String, enum: ['Attack', 'HP', 'Support', 'Defense', 'Material'] },
   base_stars: { type: Number, min: 1, max: 6 },
-  skills: [skillSchema], // Eingebettete Skills
+  skills: [skillSchema],
 });
 
 module.exports = mongoose.model('Monster', monsterSchema);
