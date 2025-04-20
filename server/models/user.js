5c874350a0a4f5e0408dfb22c9f073152e8c984b
Zanix26 <kamillanz@gmx.net>
2025-04-20 02:00:51 +0200

    user.js aktualisieren

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true }, // Korrektur: uniquie -> unique
-    email: { type: String, required: true, unique: true },    // Korrektur: uniquie -> unique
-    password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
+
+// Passwort vor dem Speichern hashen
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// Methode zum Vergleichen des Passworts
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
