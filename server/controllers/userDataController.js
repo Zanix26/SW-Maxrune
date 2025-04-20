1986925e0eb6c181b90873bc69fb454dd0d43126
Zanix26 <kamillanz@gmx.net>
2025-04-20 22:07:03 +0200

    userDataController.js aktualisieren

--- server/controllers/userDataController.js
+++ server/controllers/userDataController.js
@@ -1,16 +1,5 @@
-const mongoose = require('mongoose');
 const fs = require('fs');
-
-// Schema für Benutzer-Daten (aus SWEX-JSON)
-const userDataSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  runes: [{ type: Object }],
-  artifacts: [{ type: Object }],
-  monsters: [{ type: Object }],
-  uploadedAt: { type: Date, default: Date.now },
-});
-
-const UserData = mongoose.model('UserData', userDataSchema);
+const UserData = require('../models/userData'); // Importieren des Modells
 
 // JSON-Datei hochladen
 const uploadJson = async (req, res) => {
