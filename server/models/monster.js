5d7045d15c4fcbd2abe88eb9e04d39cd93ff5f2e
Zanix26 <kamillanz@gmx.net>
2025-04-20 16:09:15 +0200

    monster.js aktualisieren

--- server/models/monster.js
+++ server/models/monster.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
+const effectSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  is_buff: { type: Boolean },
+  type: { type: String },
+  description: { type: String },
+  icon_filename: { type: String },
+});
+
 const skillSchema = new mongoose.Schema({
   id: { type: Number, required: true },
   name: { type: String, required: true },
   description: { type: String },
   multiplier_formula: { type: String },
-  effects: [
-    {
-      effect: { type: String },
-      chance: { type: Number },
-    },
-  ],
+  effects: [effectSchema], // Angepasstes Schema für effects
 });
 
 const monsterSchema = new mongoose.Schema({
