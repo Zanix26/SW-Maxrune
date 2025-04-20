d8dfb5c8032896765380993afdf0945a411919dc
Zanix26 <kamillanz@gmx.net>
2025-04-20 23:19:03 +0200

    server.js aktualisieren

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ const http = require('http'); // Neu: HTTP-Modul für mehr Kontrolle
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Erhöhe die maximale Anfragegröße
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
 // Middleware
 app.use(cors({
   origin: 'https://optimizer.stocktropia.com',
